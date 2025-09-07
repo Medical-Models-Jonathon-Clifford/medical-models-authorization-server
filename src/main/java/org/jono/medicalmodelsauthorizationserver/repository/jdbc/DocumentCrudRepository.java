d0728e995534af499a1127da644450d5a989d41e
JonathonClifford <jono.cliff@gmail.com>
2025-09-07 18:22:33 +1000

    chore: MM-95 Removing unneeded repositories.

--- src/main/java/org/jono/medicalmodelsauthorizationserver/repository/jdbc/DocumentCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package org.jono.medicalmodelsauthorizationserver.repository.jdbc;
-
-import org.jono.medicalmodelsauthorizationserver.model.Document;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DocumentCrudRepository extends CrudRepository<Document, String> {
-}
--- src/main/java/org/jono/medicalmodelsauthorizationserver/repository/jdbc/DocumentRelationshipCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package org.jono.medicalmodelsauthorizationserver.repository.jdbc;
-
-import org.jono.medicalmodelsauthorizationserver.model.DocumentRelationship;
-import org.springframework.data.repository.CrudRepository;
-
-public interface DocumentRelationshipCrudRepository extends CrudRepository<DocumentRelationship, String> {
-}
