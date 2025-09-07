39a202758aa73bdf25e8cbfddcf822b467c8ccdd
JonathonClifford <jono.cliff@gmail.com>
2025-09-07 18:19:43 +1000

    chore: MM-95 Remove unneeded document and comment tests.

--- src/main/java/org/jono/medicalmodelsauthorizationserver/repository/jdbc/CommentCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package org.jono.medicalmodelsauthorizationserver.repository.jdbc;
-
-import java.util.List;
-import org.jono.medicalmodelsauthorizationserver.model.Comment;
-import org.springframework.data.repository.CrudRepository;
-
-public interface CommentCrudRepository extends CrudRepository<Comment, String> {
-    List<Comment> findAllByDocumentId(String documentId);
-}
