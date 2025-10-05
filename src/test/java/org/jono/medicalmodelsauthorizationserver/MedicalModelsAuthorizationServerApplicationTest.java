26517d32056c98108cd2f134c8aeac8bf83d8fa6
JonathonClifford <jono.cliff@gmail.com>
2025-10-05 05:02:06 +0000

    test: MM-100 Add first Spring Integration test. (#28)
    Reviewed-on: https://gitea.busybunyip.com/medical-models/medical-models-authorization-server/pulls/28
    Co-authored-by: JonathonClifford <jono.cliff@gmail.com>
    Co-committed-by: JonathonClifford <jono.cliff@gmail.com>

---
+++ src/test/java/org/jono/medicalmodelsauthorizationserver/MedicalModelsAuthorizationServerApplicationTest.java
@@ -0,0 +1,12 @@
+package org.jono.medicalmodelsauthorizationserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicalModelsAuthorizationServerApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
